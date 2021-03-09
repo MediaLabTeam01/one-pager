56491272b124cb83b3ffe03ca621ba744374d435
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-03-09 10:43:28 +0100

    Added logo

--- src/components/topHeader/topHeader.js
+++ src/components/topHeader/topHeader.js
@@ -3,6 +3,7 @@ import './topHeader.css';
 function TopHeader() {
     return (
       <div className="top-header">
+          <img  className="logo" src={process.env.PUBLIC_URL + '/BG-Icon.png'} alt={'BrainGem Logo'} /> 
           <h2> MediaLab SmartCity - Team 1 </h2>
       </div>
     );
