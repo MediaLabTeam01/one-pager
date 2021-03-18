99bccb83d436b03e9c5abf524a20de986b7bd3ca
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-03-18 22:11:36 +0100

    Fixed multiple issues

--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -3,8 +3,15 @@ import './navBar.css';
 function NavBar() {
     return (
       <div className="navbar">
-        <div className="right-logo">
+        <div className="left-block">
             <img  className="logo" src={process.env.PUBLIC_URL + '/BG-Icon.png'} alt={'BrainGem Logo'} /> 
+            <p className="team-name">BrainGem</p>
+        </div>
+        <div className="right-block">
+            <p className="navBarButton">Team</p>
+            <p className="navBarButton">Deliverables</p>
+            <p className="navBarButton">Concept</p>
+            <p className="navBarButton">Contact</p>
         </div>
       </div>
     );
