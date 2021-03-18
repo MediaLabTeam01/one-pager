376fec84e1db32642f92a5b8890ee8f388d6884d
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-03-18 21:41:07 +0100

    Added color from design

--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -2,8 +2,10 @@ import './navBar.css';
 
 function NavBar() {
     return (
-      <div className="right-logo">
-          <img  className="logo" src={process.env.PUBLIC_URL + '/BG-Icon.png'} alt={'BrainGem Logo'} /> 
+      <div className="navbar">
+        <div className="right-logo">
+            <img  className="logo" src={process.env.PUBLIC_URL + '/BG-Icon.png'} alt={'BrainGem Logo'} /> 
+        </div>
       </div>
     );
   }
