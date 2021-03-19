25704fd6f58f98d7e7705e0e17cbdec45cc72f4f
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-03-19 17:20:28 +0100

    Fixed header title

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
       <div className="header">  
             <img className="header-image" src={process.env.PUBLIC_URL + '/header.png'} alt={'Header'}/>
-            <p className="title">Wij designen voor een schoner en gezonder Scheveningen</p>
+            <p className="title">Wij designen voor een schoner <br></br> en gezonder <span className="scheveningen"> Scheveningen</span></p>
       </div>
     );
   }
