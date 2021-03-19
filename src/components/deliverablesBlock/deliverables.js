8dbbcc02942a7a30b70af4ceb630bba848a45380
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-03-19 20:29:36 +0100

    added new deliverables

--- src/components/deliverablesBlock/deliverables.js
+++ src/components/deliverablesBlock/deliverables.js
@@ -5,9 +5,27 @@ function Deliverables() {
     return (
       <div id="deliverable" className="deliv">
           <h3 className="deliv-title">Deliverables</h3>
+            <div className="block-container">
+                <div className="del-block">
+                  <h4 className="del-title">Opdracht van Living Labs Scheveningen:</h4>
+                  <div className="del-left">
+                    <p className="question">Hoe kan een slimme toepassing eruitzien, waarmee de leefomgeving en beleving van bewoners wordt verbeterd of verrijkt?</p>
+                    <img className="theme" src={process.env.PUBLIC_URL + '/theme.png'} alt={'Deliverable'}/>
+                </div>
+              </div>
+              <DeliverableBlock title={'Inzichten desk- en fieldresearch'} image={'o-inzichten.png'} />
+          </div>
+          <img className="divider-dash" src={process.env.PUBLIC_URL + '/dash-medium.png'} alt={'mediumdash'}/>
           <div className="block-container">
+            <div className="del-block">
+                  <h4 className="del-title">Onwerpopgave</h4>
+                  <div className="del-left">
+                    <p className="question">Hoe kunnen we de mens en natuur samen vreedzaam laten leven door middel van een slimme oplossing?</p>
+                    <h4 className="del-title">Enquete onderzoek</h4>
+                    <img className="theme" src={process.env.PUBLIC_URL + '/graph.png'} alt={'Deliverable'}/>
+                </div>
+              </div>
               <DeliverableBlock title={'Design Principles'} image={'designprinciples.png'} />
-              <DeliverableBlock title={'Onderzoeks richtingen'} image={'o-inzichten.png'} />
           </div>
       </div>
     );
--- src/components/teamMemberBlock/teamMemberBlock.js
+++ src/components/teamMemberBlock/teamMemberBlock.js
@@ -3,7 +3,7 @@ import './teamMemberBlock.css';
 function teamMemberBlock(props) {
     return (
       <div className="tm-block">
-          <img className="tm-image" src={process.env.PUBLIC_URL + '/BG-Icon.png'} alt={'BrainGem Logo'}/>
+          <img className="tm-image" src={process.env.PUBLIC_URL + '/logo.png'} alt={'BrainGem Logo'}/>
           <div className="tm-info">
             <p className="tm-name"> {props.name} </p>
             <p className="tm-course"> {props.course} </p>
