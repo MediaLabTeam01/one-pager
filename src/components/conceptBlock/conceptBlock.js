d39d94724bca72293af47814d627ff3e882f2c76
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-03-21 11:03:39 +0100

    Added concept explenation

--- src/components/conceptBlock/conceptBlock.js
+++ src/components/conceptBlock/conceptBlock.js
@@ -7,7 +7,17 @@ function Concept() {
           <h3 className="c-title">Het concept</h3>
           <div className="c-content">
             <div className="block-container">
-              <div className="c-blocks"> <p>Toelichting</p></div>
+              <div className="c-blocks"> 
+                <p className="explanation">
+                  De BinoculAR is een verrekijker die werkt met augmented reality. Met
+                  deze verrekijker ziet de gebruiker de natuur op een heel andere
+                  manier. Als de gebruiker door de verrekijker kijkt worden verschillende
+                  details van de planten om hem heen zichtbaar. Ook projecteert de BinoculAR verschillende dieren die voorkomen in het gebied. Als de gebruiker een dier of plantensoort ziet waar hij meer van wil weten kan hij
+                  deze scannen door er voor een paar seconden naar te kijken, en vervolgens verschijnt er meer informatie die door middel van augmented reality om het dier of de plant heen wordt geprojecteerd. Maar de dieren
+                  bewegen wel, dit geeft een uitdaging het dier goed in zicht te krijgen
+                  voor informatie
+                </p>
+              </div>
               <div className="c-blocks"> <p>Beeldmateriaal</p> </div>
             </div>
           </div>
