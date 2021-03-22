6dcd67d052a9aaba7ccef040ac3d79a18be6aae8
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-03-22 16:54:34 +0100

    Added condition in team member blocks

--- src/components/teamMemberBlock/teamMemberBlock.js
+++ src/components/teamMemberBlock/teamMemberBlock.js
@@ -1,6 +1,17 @@
 import './teamMemberBlock.css';
 
 function teamMemberBlock(props) {
+  if (props.name === "Wessel") {
+    return (
+      <div className="tm-block">
+          <img className="tm-image" src={process.env.PUBLIC_URL + '/teamMembers/' + props.name + '.jpeg'} alt={props.name}/>
+          <div className="tm-info">
+            <p className="tm-name"> {props.name} </p>
+            <p className="tm-course"> {props.course} </p>
+          </div>
+      </div>
+    );
+  }
     return (
       <div className="tm-block">
           <img className="tm-image" src={process.env.PUBLIC_URL + '/teamMembers/' + props.name + '.jpg'} alt={props.name}/>
